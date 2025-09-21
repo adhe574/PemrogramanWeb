e3f3a8ec29d58b4c2024a674644193a6b3b3f21e
Adhe Widya <adhewidya378@gmail.com>
2025-09-21 08:55:18 +0700

    simpan

--- Jobsheet 4 PHP 01/struktur_kontrol.php
+++ Jobsheet 4 PHP 01/struktur_kontrol.php
@@ -10,7 +10,7 @@ if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
 } elseif ($nilaiNumerik < 70) {
     echo "Nilai huruf : D";
 }
-echo "<br><br>";
+echo "<br>";
 
 $jarakSaatIni = 0;
 $jarakTarget = 500;
@@ -23,4 +23,62 @@ while ($jarakSaatIni < $jarakTarget) {
 }
 
 echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer. <br>";
+
+$jumlahLahan = 10;
+$tanamanPerLahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i = 1; $i <= $jumlahLahan; $i++) {
+    $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);    
+}
+echo "Jumlah buah yang akan dipanen adalah : $jumlahBuah <br>";
+
+$skorUjian = [85, 92, 78, 96, 88];
+$totalSkor = 0;
+
+foreach ($skorUjian as $skor) {
+    $totalSkor += $skor;
+}
+
+echo "Total skor ujian adalah : $totalSkor <br>";
+
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai < 60) {
+        echo "Nilai : $nilai (Tidak Lulus) <br>";
+        continue;
+    } 
+        echo "Nilai : $nilai (Lulus) <br>";
+}
+
+$daftarNilai = [85, 92, 78, 64, 90, 75, 88, 79, 70, 96];
+sort($daftarNilai);
+$jumlahSiswa = count($daftarNilai) - 4;
+$totalNilai = 0;
+for ($i = 2; $i < count ($daftarNilai); $i++) {
+    $totalNilai += $daftarNilai[$i];
+}
+
+$rataRataNilai = $totalNilai / $jumlahSiswa;
+
+echo "Total nilai setelah menghapus dua nilai tertinggi dan dua nilai terendah adalah : $totalNilai <br>";
+echo "Rata-rata nilai setelah menghapus dua nilai tertinggi dan dua nilai terendah adalah : $rataRataNilai <br>";
+
+$harga = 120000;
+$diskon = 0;
+
+if ($harga > 100000) {
+    $diskon = 0.2 * $harga;
+}
+
+$hargaSetelahDiskon = $harga - $diskon;
+echo "Harga setelah diskon adalah : $hargaSetelahDiskon <br>";
+
+$poin = 510;
+
+echo "Skor pemain adalah : $poin <br>";
+$hadiah = ($poin > 500) ? "Ya" : "Tidak";
+echo "Apakah pemain mendapatkan hadiah? $hadiah <br>";
 ?>
